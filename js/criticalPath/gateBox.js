6777367eb7bee73fced9835c45c7004d4a2ab592
baconadmiral <andrewwilliams1087@gmail.com>
2018-09-24 15:23:35 -0600

    added flow counter

--- js/criticalPath/gateBox.js
+++ js/criticalPath/gateBox.js
@@ -1,5 +1,6 @@
 function GateBox(sketch, posX, posY, description, flow, afterBoxList)
 {
+  this.clicked = false;
   this.posX = posX;
   this.posY = posY;
 
@@ -16,8 +17,11 @@ function GateBox(sketch, posX, posY, description, flow, afterBoxList)
 
   this.update = function()
   {
+    sketch.textSize(12);
+    sketch.fill(0);
     sketch.text(this.description, this.posX + 5, this.posY + this.h/3);
     sketch.text("Flow: " + this.flow, this.posX + 5, this.posY + this.h/2 + this.h/4);
+
     sketch.fill(this.r, this.g, this.b, 127);
     sketch.rect(this.posX, this.posY, this.w, this.h, roundedCorner);
 
@@ -39,5 +43,21 @@ function GateBox(sketch, posX, posY, description, flow, afterBoxList)
   this.drawLines = function(afterBox)
   {
     sketch.line(this.posX+this.w/2, this.posY+this.h, afterBox.posX+this.w/2, afterBox.posY);
+  }
+
+  this.click = function()
+  {
+    this.r = 204;
+    this.g = 102;
+    this.b = 0;
+    this.clicked = true;
+  }
+
+  this.unclick = function()
+  {
+    this.r = 10;
+    this.g = 10;
+    this.b = 10;
+    this.clicked = false;
   }
 }
