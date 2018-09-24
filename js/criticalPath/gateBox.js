2eda4d62e97983157100a00f5b5b54bb9aca0bd0
baconadmiral <andrewwilliams1087@gmail.com>
2018-09-24 12:47:00 -0600

    Organized and added flow to critical path activity

--- js/criticalPath/gateBox.js
+++ js/criticalPath/gateBox.js
@@ -1,7 +1,10 @@
-function GateBox(sketch, posX, posY, afterBoxList)
+function GateBox(sketch, posX, posY, description, flow, afterBoxList)
 {
   this.posX = posX;
   this.posY = posY;
+
+  this.description = description;
+  this.flow = flow;
 
   this.r = 10;
   this.g = 10;
@@ -13,14 +16,24 @@ function GateBox(sketch, posX, posY, afterBoxList)
 
   this.update = function()
   {
+    sketch.text(this.description, this.posX + 5, this.posY + this.h/3);
+    sketch.text("Flow: " + this.flow, this.posX + 5, this.posY + this.h/2 + this.h/4);
     sketch.fill(this.r, this.g, this.b, 127);
     sketch.rect(this.posX, this.posY, this.w, this.h, roundedCorner);
+
+
     if(typeof afterBoxList != 'undefined')
+    if($.isArray(afterBoxList))
+    {
       for(let i = 0; i < afterBoxList.length; i++)
       {
         if(afterBoxList[i] != this)
           this.drawLines(afterBoxList[i]);
       }
+    }
+    else {
+      this.drawLines(afterBoxList);
+    }
   }
 
   this.drawLines = function(afterBox)
