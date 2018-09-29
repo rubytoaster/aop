62d3d3522bbbb683341ed6a9664049a3ba948dcd
baconadmiral <andrewwilliams1087@gmail.com>
2018-09-29 12:35:24 -0600

    Added new images to the critical path activity

--- js/criticalPath/gateBox.js
+++ js/criticalPath/gateBox.js
@@ -1,4 +1,4 @@
-function GateBox(sketch, posX, posY, description, flow, afterBoxList, waitForBoxList)
+function GateBox(sketch, posX, posY, description, flow, imageOnList, imageOff, imgScaleWidth, imageScaleHeight, afterBoxList, waitForBoxList)
 {
   this.hasPower = false;
   this.waitForBoxList;
@@ -12,21 +12,21 @@ function GateBox(sketch, posX, posY, description, flow, afterBoxList, waitForBox
   this.h = 60;
   this.w = 60;
   var roundedCorner = 10;
-  this.beakerImgOff = sketch.loadImage("images/game/criticalPath/beakerOff.png");
-  this.beakerImg1 = sketch.loadImage("images/game/criticalPath/beaker1.png");
-  this.beakerImg2 = sketch.loadImage("images/game/criticalPath/beaker2.png");
-  this.beakerImg3 = sketch.loadImage("images/game/criticalPath/beaker3.png");
-  this.beakerList = [];
-  this.beakerList.push(this.beakerImg1);
-  this.beakerList.push(this.beakerImg2);
-  this.beakerList.push(this.beakerImg3);
+  
+  this.imageOnList = imageOnList;
+  this.imageOff = imageOff;
   
   this.animIndex = 0;
+  
+  this.imageScaleY = 45;
+  this.imageScaleY = 25;
 
   this.update = function()
   {
+
+    
     sketch.textSize(12);
-    sketch.text("Flow: " + this.flow, this.posX + 5, this.posY + this.h-4);
+    sketch.text("Flow: " + this.flow + " s", this.posX + 5, this.posY + this.h-4);
     //sketch.rect(this.posX, this.posY, this.w, this.h, roundedCorner);
     
     if(sketch.frameCount % 5 == 0)
@@ -39,10 +39,10 @@ function GateBox(sketch, posX, posY, description, flow, afterBoxList, waitForBox
     
     if(this.hasPower)
     {
-      sketch.image(this.beakerList[this.animIndex], this.posX + 14, this.posY, 25, 45);
+      sketch.image(this.imageOnList[this.animIndex], this.posX + 14, this.posY, imgScaleWidth, imageScaleHeight);
     }
     else
-      sketch.image(this.beakerImgOff, this.posX + 14, this.posY, 25, 45);
+      sketch.image(this.imageOff, this.posX + 14, this.posY, imgScaleWidth, imageScaleHeight);
 
     if(typeof afterBoxList != 'undefined' && afterBoxList != null)
     if($.isArray(afterBoxList))
--- js/criticalPath/sketch.js
+++ js/criticalPath/sketch.js
@@ -7,8 +7,18 @@ var cPathSim = function(sketch) {
   var flowTotal = 0;
   let mouseSlower = false;//This is to prevent mouse bounces since we cant use mouse clicked
   let mouseSlowerCtr = 0;
+  let beakerImgOff;
+  let gumballImgOff;
+  let birdImgOff;
+  let beakerImgList = [];
+  let birdImgList = [];
+  let gumballImgList = []
 
   sketch.setup = function() {
+    
+    loadImages();
+    
+    
     sketch.frameRate(30);
     can = sketch.createCanvas(400, 500);
     const canvasElt = can.elt;
@@ -17,45 +27,45 @@ var cPathSim = function(sketch) {
     var centerBox = sketch.width/2 - defaultWidth/2;
 
     //box list needs to be constructed backwards
-    var sendBox = new GateBox(sketch, centerBox, 400, "Send", 2, null, null);
+    var sendBox = new GateBox(sketch, centerBox, 400, "Send", 2, beakerImgList, beakerImgOff, 25, 45, null, null);
     gateBoxList.push(sendBox);
 
-    var paintBox = new GateBox(sketch, centerBox, sendBox.posY - defaultBoxSpacing, "Painting", 2, sendBox);
+    var paintBox = new GateBox(sketch, centerBox, sendBox.posY - defaultBoxSpacing, "Painting", 2, beakerImgList, beakerImgOff,  25, 45, sendBox);
     gateBoxList.push(paintBox);
     sendBoxWaitList.push(paintBox);
 
-    var sandingBox = new GateBox(sketch, centerBox,paintBox.posY - defaultBoxSpacing, "Sanding", 3, paintBox);
+    var sandingBox = new GateBox(sketch, centerBox,paintBox.posY - defaultBoxSpacing, "Sanding", 3, gumballImgList, gumballImgOff, 25, 45,paintBox);
     gateBoxList.push(sandingBox);
 
-    var washBox = new GateBox(sketch, centerBox, sandingBox.posY - defaultBoxSpacing, "Washing", 1, sandingBox);
+    var washBox = new GateBox(sketch, centerBox, sandingBox.posY - defaultBoxSpacing, "Washing", 1, birdImgList, birdImgOff, 45, 45,sandingBox);
     gateBoxList.push(washBox);
 
     var rootPathList = [];
     rootPathList.push(washBox);
 
     //Not critical path boxes
-    var hammeringBox = new GateBox(sketch, centerBox-defaultBoxSpacingWidth, paintBox.posY - defaultBoxSpacing, "Nailing", 5, sendBox);
+    var hammeringBox = new GateBox(sketch, centerBox-defaultBoxSpacingWidth, paintBox.posY - defaultBoxSpacing, "Nailing", 5, gumballImgList, gumballImgOff, 25, 45,sendBox);
     gateBoxList.push(hammeringBox);
     sendBoxWaitList.push(hammeringBox);
 
-    var flatteningBox = new GateBox(sketch, centerBox-defaultBoxSpacingWidth, hammeringBox.posY - defaultBoxSpacing, "Flattening", 4, hammeringBox);
+    var flatteningBox = new GateBox(sketch, centerBox-defaultBoxSpacingWidth, hammeringBox.posY - defaultBoxSpacing, "Flattening", 4, beakerImgList, beakerImgOff, 25, 45,hammeringBox);
     gateBoxList.push(flatteningBox);
 
     rootPathList.push(flatteningBox);
 
     //Critical Path Boxes
-    var rivetingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, sendBox.posY - defaultBoxSpacing, "Welding", 1, sendBox);
+    var rivetingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, sendBox.posY - defaultBoxSpacing, "Welding", 1, gumballImgList, gumballImgOff, 25, 45,sendBox);
     gateBoxList.push(rivetingBox);
     sendBoxWaitList.push(rivetingBox);
 
-    var weldingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, rivetingBox.posY - defaultBoxSpacing, "Riveting", 2, rivetingBox);
+    var weldingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, rivetingBox.posY - defaultBoxSpacing, "Riveting", 2, beakerImgList, beakerImgOff, 25, 45,rivetingBox);
     gateBoxList.push(weldingBox);
-    var gluingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, weldingBox.posY - defaultBoxSpacing, "Gluing", 5, weldingBox);
+    var gluingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, weldingBox.posY - defaultBoxSpacing, "Gluing", 5, birdImgList, birdImgOff, 45, 45,weldingBox);
     gateBoxList.push(gluingBox);
 
     rootPathList.push(gluingBox);
 
-    var receiveBox = new GateBox(sketch, centerBox, washBox.posY - defaultBoxSpacing, "Receive", 2, rootPathList);
+    var receiveBox = new GateBox(sketch, centerBox, washBox.posY - defaultBoxSpacing, "Receive", 2, birdImgList, birdImgOff, 45, 45,rootPathList);
     gateBoxList.push(receiveBox);
     
     sendBox.setWaitForBoxList(sendBoxWaitList);
@@ -111,16 +121,35 @@ var cPathSim = function(sketch) {
             gateBoxList[i].powerOn();
             
           }
-        /*  else {
-            flowTotal -= gateBoxList[i].flow;
-            gateBoxList[i].unclick();
-            gateBoxList[i].powerOff();
-          }*/
-
         }
       }
       
     }
+  }
+  
+  function loadImages()
+  {
+    beakerImgOff = sketch.loadImage("images/game/criticalPath/beakerOff.png");
+    
+    beakerImgList.push(sketch.loadImage("images/game/criticalPath/beaker1.png"));
+    beakerImgList.push(sketch.loadImage("images/game/criticalPath/beaker2.png"));
+    beakerImgList.push(sketch.loadImage("images/game/criticalPath/beaker3.png"));
+    
+    birdImgOff = sketch.loadImage("images/game/criticalPath/birdOff.png");
+    
+    birdImgList.push(sketch.loadImage("images/game/criticalPath/bird1.png"));
+    birdImgList.push(sketch.loadImage("images/game/criticalPath/bird2.png"));
+    birdImgList.push(sketch.loadImage("images/game/criticalPath/bird3.png"));
+    
+    gumballImgOff = sketch.loadImage("images/game/criticalPath/gumballOff.png");
+    
+    gumballImgList.push(sketch.loadImage("images/game/criticalPath/gumball1.png"));
+    gumballImgList.push(sketch.loadImage("images/game/criticalPath/gumball2.png"));
+    gumballImgList.push(sketch.loadImage("images/game/criticalPath/gumball3.png"));
+    gumballImgList.push(sketch.loadImage("images/game/criticalPath/gumball4.png"));
+    gumballImgList.push(sketch.loadImage("images/game/criticalPath/gumball5.png"));
+
+    
   }
 
 }
