61052ab5444f0167b1f1cbc1678c00cd7735ab79
Anthony Sparrow <anthonywsparrow@gmail.com>
2018-12-01 13:46:55 -0700

    Updated draw logic to handle PMGs. These images will need replaced.

--- js/traffic/sketch.js
+++ js/traffic/sketch.js
@@ -1,10 +1,12 @@
 var trafficSim = function(sketch) {
+var carImgs = [];
 var complete = false;
 var numberOfLanes = 3;
+var nuumberOfOpenLanes = 3;
 var frameRate = 30;
-var carWidth = 90;
-var carHeight = 160;
-var velocity = 4;
+var carWidth = 50;
+var carHeight = 100;
+var velocity = 3;
 var intervalTime = 3000;
 var ySpawnWindowHeight = velocity*frameRate*(intervalTime/1000)-carHeight;
 var carLaneXs = [];
@@ -19,7 +21,7 @@ this.carList = [];
     canvasElt.style.width = '100%', canvasElt.style.height="100%";
     carLaneXs = calcLanes(numberOfLanes);
 
-    for(let i=1; i<=numberOfLanes; i++)
+    for(let i=1; i<=nuumberOfOpenLanes; i++)
     {
       addCar(i);
     }
@@ -27,18 +29,25 @@ this.carList = [];
 
   sketch.draw = function() {
     sketch.background(0);
+    console.log("Drawing " + carList.length + " cars");
+
+    var deleteIndex = null;
     for (let i=0; i<carList.length; i++)
     {
-      this.carList[i].update(velocity);
+      if (this.carList[i].update(velocity) == false)
+        deleteIndex = i;
     }
+    if (deleteIndex)
+      carList.splice(deleteIndex, 1);
   }
 
   function addCar(lane)
   {
     var laneWidth = can.width/3;
     var yStart = Math.random() * ySpawnWindowHeight;
-    this.carList.push(
-      new Car(sketch, carLaneXs[lane],
+    var carImg = carImgs[Math.floor(Math.random()*(carImgs.length))];
+    this.carList.unshift(
+      new Car(sketch, carImg, carLaneXs[lane],
       can.height+yStart, carWidth, carHeight));
     console.log("added car @ " + carLaneXs[lane] +
       ", " +can.height+yStart);
@@ -62,5 +71,17 @@ function calcLanes(numberOfLanes)
 
   function loadImages()
   {
+      carImgs[0] = sketch.loadImage("images/game/cars/1.png");
+      carImgs[1] = sketch.loadImage("images/game/cars/2.png");
+      carImgs[2] = sketch.loadImage("images/game/cars/3.png");
+      carImgs[3] = sketch.loadImage("images/game/cars/4.png");
+      carImgs[4] = sketch.loadImage("images/game/cars/5.png");
+      carImgs[5] = sketch.loadImage("images/game/cars/6.png");
+      carImgs[6] = sketch.loadImage("images/game/cars/7.png");
+      carImgs[7] = sketch.loadImage("images/game/cars/8.png");
+      carImgs[8] = sketch.loadImage("images/game/cars/9.png");
+      carImgs[9] = sketch.loadImage("images/game/cars/10.png");
+      carImgs[10] = sketch.loadImage("images/game/cars/11.png");
+      carImgs[11] = sketch.loadImage("images/game/cars/12.png");
   }
 }
