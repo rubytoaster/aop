fda968365edc3971cca38907ebfb4fb74325b301
Anthony Sparrow <anthonywsparrow@gmail.com>
2018-12-02 13:11:06 -0700

    Minor updates

--- js/traffic/sketch.js
+++ js/traffic/sketch.js
@@ -2,20 +2,22 @@ var trafficSim = function(sketch) {
   var carImgs = [];
   var complete = false;
   var numberOfLanes = 3;
+  var numberOfOpenLanes = 3;
   var frameRate = 30;
   var carWidth = 50;
   var carHeight = 100;
-  var velocity = 30;
-  var intervalTime = 400;
-  var ySpawnWindowHeight = velocity*frameRate*(intervalTime/1000)-carHeight;
+  var velocity = 15;
+  var intervalTime = 800;
+  var canvasWidth = 360;
+  var canvasHeight = 640;
+  var ySpawnWindowHeight = velocity * frameRate * (intervalTime / 1000) - carHeight;
   var carLaneXs = [];
   var finishedCarCount = 0;
   this.carList = [];
 
-  startCars = function(numberOfOpenLanes)
-  {
-    for(let i=1; i<=numberOfOpenLanes; i++)
-    {
+  startCars = function(numberOfOpenLanes) {
+    this.numberOfOpenLanes = numberOfOpenLanes;
+    for (let i = 1; i <= numberOfOpenLanes; i++) {
       addCar(i);
     }
   }
@@ -25,96 +27,103 @@ var trafficSim = function(sketch) {
     loadImages();
 
     sketch.frameRate(frameRate);
-    can = sketch.createCanvas(360, 640);
+    can = sketch.createCanvas(canvasWidth, canvasHeight);
     const canvasElt = can.elt;
-          canvasElt.style.width = '100%', canvasElt.style.height="100%";
+    canvasElt.style.width = '100%', canvasElt.style.height = "100%";
     carLaneXs = calcLanes(numberOfLanes);
   }
 
   sketch.draw = function() {
-    sketch.background(0);
+    sketch.background(this.empty_road);
 
-    for (let i=0; i<carList.length; i++)
+    for (let laneNumber = 3; laneNumber > this.numberOfOpenLanes; laneNumber--)
     {
-      if (carList[i])
-      {
-        if (this.carList[i].update(velocity) == false)
-        {
+      sketch.image(gate, (can.width / numberOfLanes) * (laneNumber - 1), 0, (can.width / numberOfLanes), 10);
+    }
+
+    sketch.image(this.tower, (can.width / numberOfLanes) - 15, -15, 30, 30);
+    sketch.image(this.tower, (can.width / numberOfLanes) * 2 - 15, -15, 30, 30);
+
+    for (let i = 0; i < carList.length; i++) {
+      if (carList[i]) {
+        if (this.carList[i].update(velocity) == false) {
           // remove car maintaining index
-          carList.splice(i,1);
+          carList.splice(i, 1);
           finishedCarCount++;
-          // console.log("Removing index: " + i);
-          // console.log("COUNT: " + finishedCarCount);
           // decriment index so that the car after the
           // one we deleted still gets rendered
           i--;
         }
       }
     }
-    $("#completionCount").text(finishedCarCount);
+    $("#completionCount").text("Finished Cars: " + finishedCarCount);
+    $("#crossScreenTime").text("Time to cross screen: " + canvasHeight/velocity);
+    $("#completionCount").text("finishedCacompleted" + finishedCarCount);
+    $("#completionCount").text("finishedCacompleted" + finishedCarCount);
+    $("#completionCount").text("finishedCacompleted" + finishedCarCount);
   }
 
-  function addCar(lane)
-  {
-    var laneWidth = can.width/3;
+  function addCar(lane) {
+    var laneWidth = can.width / 3;
     var yStart = Math.random() * ySpawnWindowHeight;
-    var carImg = carImgs[Math.floor(Math.random()*(carImgs.length))];
+    var carImg = carImgs[Math.floor(Math.random() * (carImgs.length))];
     this.carList.push(
       new Car(sketch, carImg, carLaneXs[lane],
-      can.height+yStart, carWidth, carHeight));
+        can.height + yStart, carWidth, carHeight));
 
-    if (!complete)
-    {
-      crateTimeout = setTimeout(function(){addCar(lane);}, intervalTime);
+    if (!complete) {
+      crateTimeout = setTimeout(function() {
+        addCar(lane);
+      }, intervalTime);
     }
   }
 
-function calcLanes(numberOfLanes)
-{
-  xpos = [];
-  var laneWidth = can.width/numberOfLanes;
-  for(let i=1; i<=numberOfLanes; i++)
-  {
-    xpos[i] = (i-1)*laneWidth + ((laneWidth-carWidth)/2);
+  function calcLanes(numberOfLanes) {
+    xpos = [];
+    var laneWidth = can.width / numberOfLanes;
+    for (let i = 1; i <= numberOfLanes; i++) {
+      xpos[i] = (i - 1) * laneWidth + ((laneWidth - carWidth) / 2);
+    }
+    return xpos;
   }
-  return xpos;
+
+  function loadImages() {
+    this.empty_road = sketch.loadImage("images/game/trafficSim/road/empty_road.png");
+    this.tower = sketch.loadImage("images/game/trafficSim/road/tower.png");
+    this.gate = sketch.loadImage("images/game/trafficSim/road/gate.png");
+    carImgs[0] = sketch.loadImage("images/game/trafficSim/cars/1.png");
+    carImgs[1] = sketch.loadImage("images/game/trafficSim/cars/2.png");
+    carImgs[2] = sketch.loadImage("images/game/trafficSim/cars/3.png");
+    carImgs[3] = sketch.loadImage("images/game/trafficSim/cars/4.png");
+    carImgs[4] = sketch.loadImage("images/game/trafficSim/cars/5.png");
+    carImgs[5] = sketch.loadImage("images/game/trafficSim/cars/6.png");
+    carImgs[6] = sketch.loadImage("images/game/trafficSim/cars/7.png");
+    carImgs[7] = sketch.loadImage("images/game/trafficSim/cars/8.png");
+    carImgs[8] = sketch.loadImage("images/game/trafficSim/cars/9.png");
+    carImgs[9] = sketch.loadImage("images/game/trafficSim/cars/10.png");
+    carImgs[10] = sketch.loadImage("images/game/trafficSim/cars/11.png");
+    carImgs[11] = sketch.loadImage("images/game/trafficSim/cars/12.png");
+  }
 }
 
-  function loadImages()
-  {
-      carImgs[0] = sketch.loadImage("images/game/cars/1.png");
-      carImgs[1] = sketch.loadImage("images/game/cars/2.png");
-      carImgs[2] = sketch.loadImage("images/game/cars/3.png");
-      carImgs[3] = sketch.loadImage("images/game/cars/4.png");
-      carImgs[4] = sketch.loadImage("images/game/cars/5.png");
-      carImgs[5] = sketch.loadImage("images/game/cars/6.png");
-      carImgs[6] = sketch.loadImage("images/game/cars/7.png");
-      carImgs[7] = sketch.loadImage("images/game/cars/8.png");
-      carImgs[8] = sketch.loadImage("images/game/cars/9.png");
-      carImgs[9] = sketch.loadImage("images/game/cars/10.png");
-      carImgs[10] = sketch.loadImage("images/game/cars/11.png");
-      carImgs[11] = sketch.loadImage("images/game/cars/12.png");
-  }
-}
-
-function loadTrafficModal(){
+function loadTrafficModal() {
   console.log("loadTrafficModal()");
-  var defaultColor= "#424242";
+  var defaultColor = "#424242";
   var pageTitle = $("#pageTitle").text();
   console.log(pageTitle);
-  if(pageTitle === "Traffic Simulator"){
+  if (pageTitle === "Traffic Simulator") {
     console.log("populating modal");
     $("#traffic_modal_content").load("content/trafficModal.html");
 
-    $(document).ready(function(){
-      $('#traffic_modal_content').modal();
-    });
-
-    $('#traffic_modal_content').modal({
-      dismissible:false
-    });
-
-    $('#traffic_modal_content').modal('open');
+    // $(document).ready(function(){
+    //   $('#traffic_modal_content').modal();
+    // });
+    //
+    // $('#traffic_modal_content').modal({
+    //   dismissible:false
+    // });
+    //
+    // $('#traffic_modal_content').modal('open');
 
   }
 }
